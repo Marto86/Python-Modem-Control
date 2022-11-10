9aba5426ba0bb41acdc50c2c20546e62eed5be50
= <=>
2022-11-10 15:58:24 +0200

    Graphical result

--- 4GtestBench/antena.py
+++ 4GtestBench/antena.py
@@ -1,15 +1,21 @@
-from rtlsdr import *
-
 from pylab import *
+from rtlsdr import *
+from matplotlib import pyplot
 
 sdr = RtlSdr()
 
-sdr.sample_rate = 3.2e6
+# configure device
+sdr.sample_rate = 2.4e6
+sdr.center_freq = 95e6
+sdr.gain = 4
 
-sdr.center_freq = 900e6
+samples = sdr.read_samples(256*1024)
+print(samples)
+sdr.close()
 
-sdr.gain = 5
+# use matplotlib to estimate and plot the PSD
+pyplot.psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+# pyplot.xlabel('Frequency (MHz)')
+# pyplot.ylabel('Relative power (dB)')
 
-samples = sdr.read_samples(500e3)
-
-print ('relative power: %0.1f dB' % (10*log10(var(samples))))
+pyplot.show()
